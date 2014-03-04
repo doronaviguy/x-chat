faed0520c49ffa046ba3aae727072867f111366a
doronaviguy <doron@spot.im>
2014-03-04 09:01:49 +0200

    ?

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
-var express = require('express'),
-	app = express(),
-	http = require('http'),
-	server = http.createServer(app),
-	websockets = require('./websockets').init(server);
+//var express = require('express'),
+//	app = express(),
+//	http = require('http'),
+//	server = http.createServer(app),
+//	websockets = require('./websockets').init(server);
+//
+//var port = process.env.PORT || 80;
+//
+//console.log("app listeneing on %d", port);
+//server.listen(port);
+//
+//app.use(express.static('./public'));
+//
+//// routing
+//app.get('/', function (req, res) {
+//	res.sendfile(__dirname + '/index.html');
+//});
+//
+//app.get('/:room', function (req, res) {
+//	var room = req.params.room;
+//	console.log('room----');
+//	res.sendfile(__dirname + '/index.html');
+//});
+//
+//
+//
+//
+//
+//
+//
 
-var port = process.env.PORT || 80;
+var express = require('express');
+var app = express();
+app.use(express.logger());
 
-console.log("app listeneing on %d", port);
-server.listen(port);
-
-app.use(express.static('./public'));
-
-// routing
-app.get('/', function (req, res) {
-	res.sendfile(__dirname + '/index.html');
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
-app.get('/:room', function (req, res) {
-	var room = req.params.room;
-	console.log('room----');
-	res.sendfile(__dirname + '/index.html');
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
-
-
-
-
-
-
-
