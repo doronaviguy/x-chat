31b1359436decc62224e10dc2da038e3f9bdafbb
doronaviguy <doronaviguy@gmail.com>
2014-03-14 11:35:24 +0200

    - gulp.js added to project

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+var path = {
+    js : 'public/**/*.js'
+};
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src(path.js)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src(path.js)
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch(path.j, ['lint', 'scripts']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'scripts', 'watch']);
