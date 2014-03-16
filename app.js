644c31a250d0d15bffe888f9e0e3371473ceef2f
doronaviguy <doronaviguy@gmail.com>
2014-03-16 18:09:01 +0200

    - minified version for production

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express'),
 	app = express(),
 	http = require('http'),
 	server = http.createServer(app),
-	websockets = require('./websockets').init(server);
+	websockets = require('./websockets').init(server),
+	path = require('path');
 
 
 var port = process.env.PORT || 5000;
@@ -13,16 +14,20 @@ app.use(express.static('./public'));
 
 // routing
 app.get('/', function (req, res) {
-	res.sendfile(__dirname + '/index.html');
+	res.sendfile(getIndex());
 });
 
 app.get('/:room', function (req, res) {
 	var room = req.params.room;
 	console.log('room----');
-	res.sendfile(__dirname + '/index.html');
+	res.sendfile(getIndex());
 });
 
+function getIndex () {
 
+	return process.env.envioremnt === 'production' ?
+		path.join(__dirname,'public','dist','index.html') :  path.join(__dirname,'public','index.html');
+}
 
 
 
